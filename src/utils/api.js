1e4e656a539a800ede52839a117c72d42699a1dd
Skorohliad Yelyzaveta <skoroglyadliza@gmail.com>
2025-07-03 12:50:46 +0200

    Update api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://regnard-designers-backend.onrender.com/",
+  baseURL: "https://regnard-designers-backend-production.up.railway.app/",
   //   headers: { "Content-Type": "application/json" },\
   withCredentials: true,
 });
